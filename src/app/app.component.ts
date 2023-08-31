75920690971b4278a98e16b5094f53bb9c26677c
flaviumateut <98695369+flaviumateut@users.noreply.github.com>
2023-08-31 14:19:13 +0300

    added heroes list and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-tour-of-heroes';
+  heroes: { id: number; name: string }[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.initializeData();
+  }
+
+  initializeData() {
+    this.heroes = [
+      { id: 1, name: 'windstorm' },
+      { id: 2, name: 'bombasto' },
+      { id: 3, name: 'magneta' },
+      { id: 4, name: 'tornado' },
+    ];
+  }
+
+  oddEven(num: number) {
+    if (num % 2 === 0) {
+      return 'even';
+    } else {
+      return 'odd';
+    }
+  }
 }
